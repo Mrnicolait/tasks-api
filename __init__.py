125ab3578f885fbdb83abe715355afcb7175e3b8
Mrnicolait <mrnicolait@gmail.com>
2026-05-05 19:22:53 -0300

    inicial

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    app = FastAPI(
+        title="Todo API",
+        description="API para gerenciamento de tarefas comarquitetura em camadas.",
+        version="1.0.0",
+        lifespan=lifespan,
+        contact={
+            "name": "Equipe da disciplina INF8B",
+
+        },)
+    
+    return app
